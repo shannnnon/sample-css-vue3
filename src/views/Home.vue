e8b18abbed4bbaa3e7e76ff223b90b37942a798b
shannon <13169937616@163.com>
2023-07-06 17:16:36 +0800

    CI/CD初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <div><router-link to="/animation-coutdown">Animation CountDown</router-link></div>
+    <div><router-link to="/drag-drop">Drag & Drop</router-link></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
